10dbd6514ca92eb81df2d2ced54bc5e542785567
Z1yoda <ziyoda01092007@gmail.comgit init git config  --global user.email ziyoda01092007@gmil.com>
2024-04-09 21:28:09 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputVal, setInputVal] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [editedName, setEditedName] = useState<string>("")
+  
+  const handleSubmit = () => {
+    setTodos((prev) => [...prev, inputVal]);
+    setInputVal("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-wrapper">
+      <div className="header">
+        <h1>To Do List</h1>
+      </div>
+      <div className="form-todo-wrapper">
+        <form onSubmit={handleSubmit} className="form">
+          <div className="field">
+            <input
+              type="text"
+              placeholder="Enter todo..."
+              required
+              value={inputVal}
+              onChange={(e) => {
+                setInputVal(e.target.value);
+              }}
+            />
+          </div>
+          <button className="submit">Submit</button>
+        </form>
+        <div className="item-wrapper">
+          {
+            todos && todos.map((el) => (
+                <div className="check-name">
+            {isEditing ? (
+                    <input
+                        className='editInput'
+                        type='text'
+                        value={editedName}
+                        onChange={(e) => setEditedName(e.target.value)}
+                        autoFocus
+                    />
+                ) : (
+                    <>
+                        <input
+                            type='checkbox'
+                            name=''
+                            id={'el_' }
+                            // onChange={handleChange}
+                            checked={status === 'checked'}
+                        />
+                        <label
+                            htmlFor={'el_' }
+                            // style={status === 'checked' ? { textDecoration: "line-through" } : {}}
+                        >
+                            {inputVal}
+                        </label>
+                    </>
+                )}
+          </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
